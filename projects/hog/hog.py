c4713df05474eb9437d975e7c876c29677b6003f
lx <zhangluxin1995@gmail.com>
2022-02-11 18:06:21 +0800

    project

--- projects/hog/hog.py
+++ projects/hog/hog.py
@@ -141,10 +141,13 @@ def play(strategy0, strategy1, score0=0, score1=0, dice=six_sided,
         elif who == 1:
             score1 += take_turn(strategy1(score1,score0),score0,dice,goal)
             score1 += hog_pile(score1, score0)
+        
         who = next_player(who)
     # END PROBLEM 5
     # (note that the indentation for the problem 6 prompt (***YOUR CODE HERE***) might be misleading)
     # BEGIN PROBLEM 6
+        say =  say(score0,score1)
+        
     "*** YOUR CODE HERE ***"
     # END PROBLEM 6
     return score0, score1
@@ -224,6 +227,23 @@ def announce_highest(who, last_score=0, running_high=0):
     assert who == 0 or who == 1, 'The who argument should indicate a player.'
     # BEGIN PROBLEM 7
     "*** YOUR CODE HERE ***"
+    def fn (num1,num2):
+        if who ==0 :
+            if num1 >  last_score:
+                high = num1 - last_score
+                if high >  running_high:
+                    global running_high = high
+                    print(running_high + " point(s)! That's a record gain for Player " +who +"!")
+            ls = num1
+        else:
+            if num2 > last_score:
+                high = num2 - last_score 
+                if high> running_high:
+                    global running_high = high
+                    print(running_high + " point(s)! That's a record gain for Player " +who +"!")
+            ls = num2
+
+    return fn
     # END PROBLEM 7
 
 
